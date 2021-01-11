84f846c6bfcb9cfca222fbad67402a75047c2519
aliencrossroad <einlee@gmail.com>
2021-01-11 11:45:21 +0900

    설정 둘다 넣어서

--- dataserver/src/index.ts
+++ dataserver/src/index.ts
@@ -9,9 +9,10 @@ export async function main(options: ApplicationConfig = {}) {
   await app.start();
   
   //app.bind(RestBindings.REQUEST_BODY_PARSER_OPTIONS).to({limit: '10MB'});
-  app.bind(RestBindings.REQUEST_BODY_PARSER_OPTIONS).to({
+  app.bind(RestBindings.REQUEST_BODY_PARSER_OPTIONS).to({    
     json: {limit: '10MB'},
     text: {limit: '10MB'},
+    limit: '10MB',
   }); // line added
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
