bb4fad8da11e741c90219bd80ccf42e6b4915ed7
aliencrossroad <einlee@gmail.com>
2021-01-20 11:42:53 +0900

    원복

--- dataserver/src/datasources/cameralist.datasource.ts
+++ dataserver/src/datasources/cameralist.datasource.ts
@@ -5,7 +5,7 @@ const config = {
   name: 'cameralist',
   connector: 'memory',
   localStorage: 'cameralist',
-  file: './public/data/cameralist.json'
+  file: 'cameralist.json'
 };
 
 // Observe application's life cycle to disconnect the datasource when
